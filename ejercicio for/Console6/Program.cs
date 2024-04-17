c20baca653c25745bed37c572095e39e40ed0f41
julianafailla <faillajuliana8@gmail.com>
2024-04-17 20:56:47 -0300

    ej-1-6

---
+++ ejercicio for/Console6/Program.cs
@@ -0,0 +1,21 @@
+﻿//El usuario ingresa un número N y se debe mostrar como resultado el factorial de dicho número.
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese un numero para saber su factorial: ");
+        int factorial = 1;
+        if (int.TryParse(Console.ReadLine()?.Replace(".", ","), out int numero))
+        {
+            for (int i = numero ;i>1;i--) 
+            {
+                factorial += i;
+            }
+        }
+        else 
+        {
+            Console.WriteLine("ERROR , Ingrese un numero para saber su factorial correcto: ");
+        }
+        Console.WriteLine($"el factorial es {factorial} ");
+    }
+}   
