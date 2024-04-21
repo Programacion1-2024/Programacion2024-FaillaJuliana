29e52f57b2162115b84959fcee91bb00af24f158
julianafailla <faillajuliana8@gmail.com>
2024-04-21 17:56:49 -0300

    ejercicios-while

---
+++ ejercicio for/Console7/Program.cs
@@ -0,0 +1,24 @@
+﻿//  El usuario ingresa un número entero positivo N y se deben mostrar todos los números enteros
+//  positivos pares desde 0 (incluido) hasta N.
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese un numero entero positivo y se mostraran todos los numeros enteros positivos pares desde el 0 hasta el elegido");
+        if (int.TryParse(Console.ReadLine()?.Replace(".", ","), out int entero))
+        {
+            for (int i =0; i<=entero; i +=2)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("Entrada incorrecta ");
+        }
+    }
+}
+        
---
+++ ejercicio for/ConsoleAppprueba/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese un nombre de persona, cuando ingrese un espacio vacio, se informara cuadtos nombres ha ingresado: ");
+        int contador = 0;
+        string nombre = "x";
+        while ()
+        {
+            Console.WriteLine("Nombre: ");
+            contador++;
+            if (string.IsNullOrEmpty(nombre) || double.TryParse(nombre, out _))
+            {
+                break;
+            }
+        }
+        Console.WriteLine($"Has ingresado {contador - 1} nombre: ");
+    }
+}
