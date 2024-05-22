ff56808fc289623bb82bceca5c04eeaf37d810df
julianafailla <faillajuliana8@gmail.com>
2024-05-22 17:03:25 -0300

    ejercicios-para-clase

---
+++ Niveles de registro/Niveles de registro/Ejercicio.cs
@@ -0,0 +1,68 @@
+﻿namespace Niveles_de_registro
+{
+
+    /*
+        Consigna: 
+            En este ejercicio procesará líneas de registro.
+
+            Cada línea de registro es una cadena con el siguiente formato: "[<LEVEL>]: <MESSAGE>".
+
+            Hay tres niveles de registro diferentes:
+                INFO
+                WARNING
+                ERROR
+
+            Tienes tres tareas, cada una de las cuales tomará una línea de registro y te pedirá que hagas algo con ella.
+
+            Tarea 1: Obtener mensaje de una línea de registro
+               Implemente el método ( estático ) LogLine.Message() para devolver un mensaje de línea de registro.
+                   LogLine.Message("[ERROR]: Invalid operation")
+                   // => "Invalid operation"
+
+            Tarea 2: Obtener el nivel de registro de una línea de registro
+               Implemente el método ( estático ) LogLine.LogLevel() para devolver el nivel de registro de una línea de registro, que debe devolverse en minúsculas.
+                   LogLine.LogLevel("[ERROR]: Invalid operation")
+                   // => "error"
+
+            Tarea 3: Reformatear una línea de registro
+               Implemente el método ( estático ) LogLine.Reformat() que reformatea la línea de registro, poniendo el mensaje primero y el nivel de registro después entre paréntesis.
+                   LogLine.Reformat("[INFO]: Operation completed")
+                   // => "Operation completed (info)"
+
+            TIPS: Funciones Split, IndexOf y Substring de la clase String
+   */
+
+    static class LogLine
+    {
+        public static string Message(string logLine)
+        {
+            int index = logLine.IndexOf("]: ");
+            if (index == -1)
+            {
+                return logLine;
+            }
+
+            return logLine.Substring(index + 3).Trim();
+        }
+
+        public static string LogLevel(string logLine)
+        {
+            if (logLine.StartsWith("[") && logLine.Contains("]: "))
+            {
+                int levelEndIndex = logLine.IndexOf("]: ");
+                string levelString = logLine.Substring(1, levelEndIndex - 1);
+                return levelString.ToLower();
+            }
+            return "unknown";
+        }
+
+        public static string Reformat(string logLine)
+        {
+            string message = Message(logLine.Replace("\r","").Replace("\n","").Replace("\t",""));
+            string logLevel = LogLevel(logLine);
+            message = message.Trim();
+            return $"{message} ({logLevel})";
+        }
+    }
+
+}
---
+++ Numeros de Armstrong/Numeros de Armstrong/Ejercicio.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Numeros_de_Armstrong
+{
+    /*
+        Consigna: 
+            Un número de Armstrong es un número que es la suma de sus propios dígitos, cada uno elevado a la potencia del número de dígitos.
+
+            Por ejemplo:
+                9 es un número de Armstrong, porque9 = 9^1 = 9
+                10 no es un número de Armstrong, porque10 != 1^2 + 0^2 = 1
+                153 es un número de Armstrong porque:153 = 1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+                154 no es un número de Armstrong porque:154 != 1^3 + 5^3 + 4^3 = 1 + 125 + 64 = 190
+
+            Escribe un código para determinar si un número es un número de Armstrong.
+    */
+
+    public static class ArmstrongNumbers
+    {
+        public static bool IsArmstrongNumber(int number)
+        {
+            char[] digitos = number.ToString().ToCharArray();
+            int[] digitosComoInt = new int[digitos.Length];
+            int numeroDigitos = digitosComoInt.Length;
+            int suma = 0;
+            for (int i = 0; i < digitos.Length; i++)
+            {
+                digitosComoInt[i] = (int)char.GetNumericValue(digitos[i]);
+                
+            }
+            foreach (int digito in digitosComoInt)
+            {
+                suma += (int)Math.Pow(digito, numeroDigitos);
+            }
+            if (suma == number)
+            {
+               return true;
+            }
+            else
+            {
+                return false;         
+            }
+        }
+    }
+}
