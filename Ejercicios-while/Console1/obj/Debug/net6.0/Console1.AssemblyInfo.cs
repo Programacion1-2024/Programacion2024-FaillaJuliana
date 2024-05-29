eb0007b8b34607787d13c30cd7fabce8c29ae970
julianafailla <faillajuliana8@gmail.com>
2024-05-29 18:58:20 -0300

    conjetura

--- Ejercicios-while/Console1/obj/Debug/net6.0/Console1.AssemblyInfo.cs
+++ Ejercicios-while/Console1/obj/Debug/net6.0/Console1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Console1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff56808fc289623bb82bceca5c04eeaf37d810df")]
 [assembly: System.Reflection.AssemblyProductAttribute("Console1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Console1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
